ad138aae78e4fce5b2114d26e269b0883fb53f6d
vasa3331 <vasiliymartinov@gmail.com>
2022-05-12 12:29:47 +0300

    Аутентификация + авторизация

---
+++ src/main/java/com/example/authentification/controllers/AppController.java
@@ -0,0 +1,26 @@
+package com.example.authentification.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+    @GetMapping("/authenticated")
+    public String au() {
+        return "authenticated";
+    }
+
+    @GetMapping("/client")
+    public String client() {
+        return "client";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+}
