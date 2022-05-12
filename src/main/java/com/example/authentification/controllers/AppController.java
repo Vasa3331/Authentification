09ae09baf3dd2d31b3c8ffd8d0aa1c1c5b5001f9
vasa3331 <vasiliymartinov@gmail.com>
2022-05-12 12:29:47 +0300

    Аутентификация

---
+++ src/main/java/com/example/authentification/controllers/AppController.java
@@ -0,0 +1,16 @@
+package com.example.authentification.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+    @GetMapping("/authenticated")
+    public String au() {
+        return "authenticated";
+    }
+}
